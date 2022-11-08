ff26998877d45f17a07bdb663af1ada7d6cdf46c
hafniz <42380178+hafniz@users.noreply.github.com>
2022-11-08 02:29:04 +0000

    Part 2.1

--- game.js
+++ game.js
@@ -1,32 +1,30 @@
 const SETTINGS = {
-    winScore: 7,
-
+    scoreToWinMatch: 7,
     smallFont: "10px retro",
     largeFont: "14px retro",
-    scoreboardColour: "black",
-
-    buttonColour: "white",
-    buttonTextColour: "black",
-
-    paddleSound: '/Sounds/Paddle.wav',
-    wallSound: '/Sounds/Wall.wav',
-    scoreSound: '/Sounds/Score.wav',
+    scoreboardColor: "black",
+    buttonColor: "white",
+    buttonTextColor: "black",
+    paddleSound: "/Sounds/Paddle.wav",
+    wallSound: "/Sounds/Wall.wav",
+    scoreSound: "/Sounds/Score.wav",
 
     fps: 60,
-    courtBackgroundColor: "gray",
-    courtHeight: 900,
-    courtWidth: 1200,
-    courtBorderColor: "blue",
-    courtBorderHeight: 25,
-    courtCenterLineColor: "black",
-    courtCenterLineSegmentLength: 68,
-    courtCenterLineGapLength: 34,
+    courtBackgroundColor: "green",
+    gameHeight: 900,
+    gameWidth: 1200,
+    topBottomBorderColor: "blue",
+    topBottomBorderHeight: 25,
+    centerLineColor: "black",
+    centerLineSegmentLength: 68,
+    centerLineGapLength: 34,
     paddleColor: "white",
     paddleLeftRightMargin: 34,
     paddleWidth: 40,
     paddleHeight: 120,
     ballColor: "white",
     ballRadius: 12,
+    ballSpeed: 300,
 }
 
 const PLAYERS = {
@@ -45,16 +43,14 @@ class Game {
 
         setInterval(function () {
             let currTime = Date.now();
-            let dT = (currTime - prevTime) / 1000;
-            game.draw(); // update
+            game.court.update((currTime - prevTime) / 1000);
+
+            let context = game.court.canvas.getContext("2d");
+            context.clearRect(0, 0, SETTINGS.gameWidth, SETTINGS.gameHeight);
+            game.court.draw();
+
             prevTime = currTime;
         }, 1000 / SETTINGS.fps);
-    }
-
-    draw() {
-        let context = this.court.canvas.getContext("2d");
-        context.clearRect(0, 0, SETTINGS.courtWidth, SETTINGS.courtHeight);
-        this.court.draw();
     }
 }
 
@@ -72,6 +68,10 @@ class Paddle {
         this.court = court;
     }
 
+    get collisionBox() {
+        return new Rectangle(this.x, this.y, this.width, this.height);
+    }
+
     draw() {
         let context = this.court.canvas.getContext("2d");
         context.fillStyle = SETTINGS.paddleColor;
@@ -80,7 +80,20 @@ class Paddle {
 }
 
 class PaddleController {
-
+    constructor(paddle) {
+        this.paddle = paddle;
+        this.paddle.court.canvas.addEventListener("mousemove", (e) => {
+            if (e.clientY <= SETTINGS.topBottomBorderHeight + this.paddle.height / 2) {
+                this.paddle.y = SETTINGS.topBottomBorderHeight;
+            }
+            else if (e.clientY >= SETTINGS.gameHeight - SETTINGS.topBottomBorderHeight - this.paddle.height / 2) {
+                this.paddle.y = SETTINGS.gameHeight - SETTINGS.topBottomBorderHeight - this.paddle.height;
+            }
+            else {
+                this.paddle.y = e.clientY - this.paddle.height / 2;
+            }
+        });
+    }
 }
 
 class AIController {
@@ -90,25 +103,40 @@ class AIController {
 class Court {
     constructor(canvas) {
         this.canvas = canvas;
-        this.paddle1 = new Paddle(SETTINGS.paddleLeftRightMargin, SETTINGS.courtHeight / 2 - SETTINGS.paddleHeight / 2, SETTINGS.paddleWidth, SETTINGS.paddleHeight, PLAYERS.playerOne, this);
-        this.paddle2 = new Paddle(SETTINGS.courtWidth - SETTINGS.paddleWidth - SETTINGS.paddleLeftRightMargin, SETTINGS.courtHeight / 2 - SETTINGS.paddleHeight / 2, SETTINGS.paddleWidth, SETTINGS.paddleHeight, PLAYERS.playerTwo, this);
-        this.ball = new Ball(SETTINGS.courtWidth / 2, SETTINGS.courtHeight / 2, SETTINGS.ballRadius, this);
+        this.paddle1 = new Paddle(SETTINGS.paddleLeftRightMargin, SETTINGS.gameHeight / 2 - SETTINGS.paddleHeight / 2, SETTINGS.paddleWidth, SETTINGS.paddleHeight, PLAYERS.playerOne, this);
+        this.paddle2 = new Paddle(SETTINGS.gameWidth - SETTINGS.paddleWidth - SETTINGS.paddleLeftRightMargin, SETTINGS.gameHeight / 2 - SETTINGS.paddleHeight / 2, SETTINGS.paddleWidth, SETTINGS.paddleHeight, PLAYERS.playerTwo, this);
+        this.ball = new Ball(SETTINGS.gameWidth / 2, SETTINGS.gameHeight / 2, SETTINGS.ballRadius, this);
+        this.paddleController1 = new PaddleController(this.paddle1);
+        this.paddleController2 = new PaddleController(this.paddle2);
+    }
+
+    get bounds() {
+        return {
+            upper: SETTINGS.topBottomBorderHeight,
+            lower: SETTINGS.gameHeight - SETTINGS.topBottomBorderHeight,
+            left: 0,
+            right: SETTINGS.gameWidth
+        }
+    }
+
+    update(dT) {
+        this.ball.update(dT);
     }
 
     draw() {
         let context = this.canvas.getContext("2d");
         context.fillStyle = SETTINGS.courtBackgroundColor;
-        context.fillRect(0, 0, SETTINGS.courtWidth, SETTINGS.courtHeight);
+        context.fillRect(0, 0, SETTINGS.gameWidth, SETTINGS.gameHeight);
 
-        context.fillStyle = SETTINGS.courtBorderColor;
-        context.fillRect(0, 0, SETTINGS.courtWidth, SETTINGS.courtBorderHeight);
-        context.fillRect(0, SETTINGS.courtHeight - SETTINGS.courtBorderHeight, SETTINGS.courtWidth, SETTINGS.courtBorderHeight);
+        context.fillStyle = SETTINGS.topBottomBorderColor;
+        context.fillRect(0, 0, SETTINGS.gameWidth, SETTINGS.topBottomBorderHeight);
+        context.fillRect(0, SETTINGS.gameHeight - SETTINGS.topBottomBorderHeight, SETTINGS.gameWidth, SETTINGS.topBottomBorderHeight);
 
-        context.strokeStyle = SETTINGS.courtCenterLineColor;
+        context.strokeStyle = SETTINGS.centerLineColor;
         context.beginPath();
-        context.setLineDash([SETTINGS.courtCenterLineSegmentLength, SETTINGS.courtCenterLineGapLength]);
-        context.moveTo(SETTINGS.courtWidth / 2, SETTINGS.courtBorderHeight + SETTINGS.courtCenterLineGapLength);
-        context.lineTo(SETTINGS.courtWidth / 2, SETTINGS.courtHeight - SETTINGS.courtBorderHeight);
+        context.setLineDash([SETTINGS.centerLineSegmentLength, SETTINGS.centerLineGapLength]);
+        context.moveTo(SETTINGS.gameWidth / 2, SETTINGS.topBottomBorderHeight + SETTINGS.centerLineGapLength);
+        context.lineTo(SETTINGS.gameWidth / 2, SETTINGS.gameHeight - SETTINGS.topBottomBorderHeight);
         context.stroke();
 
         this.paddle1.draw();
@@ -123,44 +151,81 @@ class Ball {
         this.y = y;
         this.radius = radius;
         this.court = court;
+        this.velocity = {
+            vx: SETTINGS.ballSpeed,
+            vy: SETTINGS.ballSpeed
+        }
+    }
+
+    get collisionBox() {
+        return new Rectangle(this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
+    }
+
+    update(dT) {
+        let paddle1 = this.court.paddle1, paddle2 = this.court.paddle2
+        if (this.collisionBox.overlaps(paddle1.collisionBox)) { // player 1 hits
+            this.velocity.vx *= -1;
+            this.x = paddle1.x + paddle1.width + this.radius;
+        }
+        else if (this.collisionBox.overlaps(paddle2.collisionBox)) { // player 2 hits
+            this.velocity.vx *= -1;
+            this.x = paddle2.x - this.radius;
+        }
+
+        if (this.y < this.court.bounds.upper + this.radius) {
+            this.velocity.vy *= -1;
+            this.y = this.court.bounds.upper + this.radius;
+        }
+        else if (this.y > this.court.bounds.lower - this.radius) {
+            this.velocity.vy *= -1;
+            this.y = this.court.bounds.lower - this.radius;
+        }
+
+        //if (this.x < this.court.bounds.left + this.radius) {
+        //    this.velocity.vx *= -1;
+        //    this.x = this.court.bounds.left + this.radius;
+        //}
+        //else if (this.x > this.court.bounds.right - this.radius) {
+        //    this.velocity.vx *= -1;
+        //    this.x = this.court.bounds.right - this.radius;
+        //}
+
+        this.x += this.velocity.vx * dT;
+        this.y += this.velocity.vy * dT;
     }
 
     draw() {
         let context = this.court.canvas.getContext("2d");
         context.fillStyle = SETTINGS.ballColor;
         context.beginPath();
-        context.arc(this.x, this.y, SETTINGS.ballRadius, 0, 2 * Math.PI);
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         context.fill();
     }
 }
 
 class Rectangle {
     constructor(x, y, width, height) {
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
     }
 
-    get left() { return this.x }
-    get right() { return this.x + this.width }
-    get top() { return this.y }
-    get bottom() { return this.y + this.height }
+    get left() { return this.x; }
+    get right() { return this.x + this.width; }
+    get top() { return this.y; }
+    get bottom() { return this.y + this.height; }
 
     overlaps(other) {
-        return other.left < this.right &&
-            this.left < other.right &&
-            other.top < this.bottom &&
-            this.top < other.bottom
+        return other.left < this.right && this.left < other.right && other.top < this.bottom && this.top < other.bottom;
     }
 
     contains(x, y) {
-        return this.left < x && this.right > x && this.top < y && this.bottom > y
+        return this.left < x && this.right > x && this.top < y && this.bottom > y;
     }
-
 }
 
-const canvas = document.getElementById("game")
-canvas.width = SETTINGS.courtWidth;
-canvas.height = SETTINGS.courtHeight;
+const canvas = document.getElementById("game");
+canvas.width = SETTINGS.gameWidth;
+canvas.height = SETTINGS.gameHeight;
 new Game(canvas).start();
